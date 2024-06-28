d94173a6c64a1af343ccb67360d9a7f373638d37
Karl <karlorjalo@gmail.com>
2024-06-28 16:18:09 +0800

    fix: host

--- backend/main.py
+++ backend/main.py
@@ -9,4 +9,4 @@ import routes.magician
 app.register_blueprint(routes.magician.magician)
 
 if __name__ == '__main__':
-  app.run(debug=True, use_reloader=True)
+  app.run(host="0.0.0.0", debug=True, use_reloader=True)
