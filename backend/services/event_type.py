d3f6f28298958ab41dfd477923a11389439f12d2
Karl <karlorjalo@gmail.com>
2024-07-20 10:50:32 +0800

    fix: Clear inp on create event

--- backend/services/event_type.py
+++ backend/services/event_type.py
@@ -5,7 +5,10 @@ conn = config.db.conn
 
 def CreateEventType(event_name, hp_effect, psion_cost):
   r = cur.execute("SELECT event_id FROM cad_event_type")
-  ids = r.fetchmany()
+  try:
+    ids = r.fetchall()
+  except:
+    ids = []
   
   event_id = randint(100000, 999999)
   while event_id in ids:  
