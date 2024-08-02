6737f49b96b1dec2ce99a14181e354bcc91770c5
Karl <karlorjalo@gmail.com>
2024-08-02 15:01:02 +0800

    feat: Integrate 6axis to attack

--- cad/cad.py
+++ cad/cad.py
@@ -1,3 +1,4 @@
+import smbus					#import SMBus module of I2C
 import threading
 import requests
 import socketio
@@ -53,6 +54,46 @@ lcd = characterlcd.Character_LCD_Mono(
     lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight
 )
 
+
+#some MPU6050 Registers and their Address
+PWR_MGMT_1   = 0x6B
+SMPLRT_DIV   = 0x19
+CONFIG       = 0x1A
+GYRO_CONFIG  = 0x1B
+INT_ENABLE   = 0x38
+ACCEL_XOUT_H = 0x3B
+ACCEL_YOUT_H = 0x3D
+ACCEL_ZOUT_H = 0x3F
+GYRO_XOUT_H  = 0x43
+GYRO_YOUT_H  = 0x45
+GYRO_ZOUT_H  = 0x47
+
+
+def MPU_Init():
+	bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
+	bus.write_byte_data(Device_Address, PWR_MGMT_1, 1)
+	bus.write_byte_data(Device_Address, CONFIG, 0)
+	bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
+	bus.write_byte_data(Device_Address, INT_ENABLE, 1)
+
+def read_raw_data(addr):
+  #Accelero and Gyro value are 16-bit
+  high = bus.read_byte_data(Device_Address, addr)
+  low = bus.read_byte_data(Device_Address, addr+1)  
+
+  value = ((high << 8) | low)
+  
+  #to get signed value from mpu6050
+  if(value > 32768):
+          value = value - 65536
+  return value
+
+
+bus = smbus.SMBus(1)
+Device_Address = 0x68
+
+MPU_Init()
+
 inBattle = False
 sentBattle = False
 backend_uri = os.environ["BACKEND_URI"]
@@ -65,6 +106,8 @@ sending = False
 inp = ""
 hp = 100
 psions = 100
+
+Ax, Ay, Gx = 0,0,0
 
 def getUserId():
   global uid 
@@ -93,6 +136,16 @@ def display(t1, t2=""):
     lcd.message = t2
     ot1 = t1
     ot2 = t2
+
+def read6axis():
+  global Ax, Ay, Gx
+  acc_x = read_raw_data(ACCEL_XOUT_H)
+  acc_y = read_raw_data(ACCEL_YOUT_H)
+  gyro_x = read_raw_data(GYRO_XOUT_H)
+
+  Ax = acc_x/16384.0
+  Ay = acc_y/16384.0
+  Gx = gyro_x/131.0
 
 def readLine(line, characters):
 	global inp
@@ -222,6 +275,9 @@ def main():
             display(f'HP: {hp} PSI: {psions}', inp)
           if inp != "":
             if inp[-1] == "A":
+              while not Ax > 1.2:
+                display("PUSH ARM")
+                read6axis()
               createEvent()
             elif inp[-1] == "C":
               inp = inp[:-2]
